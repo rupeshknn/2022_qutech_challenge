63985eb9a4d4e1de5f40cc3ac9f9316358754b6a
R K Rupesh <33568503+rupeshknn@users.noreply.github.com>
2022-02-02 11:14:34 +0530

    Update classical_channel.py

--- classical_channel.py
+++ classical_channel.py
@@ -8,14 +8,9 @@ USERS = {'AG' : '1487725830419914754',
         }
 
 def authenticate(user):
-    if user==USERS['QM']:
-        #quantum master details
-        auth = tweepy.OAuthHandler("M1JiB0ALf3SgKoLkpfo2xf1ll", "SCsDjJhNFTngxj0xusKiXiIAM80yImhu5swHVWWANENT8RyfoP")
-        auth.set_access_token("1487721422151913473-fP1zCmzRVJUItQyWFPPUprfeRnEAFj", "aXiBWJO0gcTV0vZHHIRIcEqQsu2Z9jv5YxSpTEUvVKlZo")
-    elif user==USERS['RK']:
-        #rupeshknn details
-        auth = tweepy.OAuthHandler("AS4Dvzlr27meAV6NAmYY2bITY", "duqwqGV8HSgLprZnEMiUbVO8p7klnnUHh4UqxJAxSMyw0WMRfB")
-        auth.set_access_token("942611539042877440-RG0z3ySCvPhRcfR5oek1ngHCt8Lsjcv", "5idekPwy8iBxYO3cXZUS4UfhAFseenh8uVSUGKNiZp181")
+    #quantum master details
+    auth = tweepy.OAuthHandler("M1JiB0ALf3SgKoLkpfo2xf1ll", "SCsDjJhNFTngxj0xusKiXiIAM80yImhu5swHVWWANENT8RyfoP")
+    auth.set_access_token("1487721422151913473-fP1zCmzRVJUItQyWFPPUprfeRnEAFj", "aXiBWJO0gcTV0vZHHIRIcEqQsu2Z9jv5YxSpTEUvVKlZo")
     else:
         print
     api = tweepy.API(auth)
